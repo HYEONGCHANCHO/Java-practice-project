cfdc37e9e2b0973696b2334788a36ed96d0fd3cf
HYEONGCHANCHO <118033064+HYEONGCHANCHO@users.noreply.github.com>
2022-11-18 18:20:19 +0900

    생활코딩 java 연습

---
+++ StringApp.java
@@ -0,0 +1,13 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); //String 문자열 큰 따옴표
+		System.out.println("H"); //Character 한글자 작은 따옴표
+		
+		System.out.println("Hello\nWorld"); //줄바꿈
+		System.out.println("Hello \"World\""); //Hello "World"
+
+	}
+
+}
