e7eb388021afceb4da79e576d4e6edd5b0dfad26
HYEONGCHANCHO <118033064+HYEONGCHANCHO@users.noreply.github.com>
2022-11-28 19:42:08 +0900

    java1 12.1강의까지 수정

---
+++ java1/Data_and_operation/letter.java
@@ -0,0 +1,13 @@
+
+public class letter {
+
+	public static void main(String[] args) {
+
+		String name = "leezche";
+		System.out.println("Hello, "+name+"");
+		
+		double VAT = 10.0;
+				System.out.println(VAT);
+	}
+
+}
